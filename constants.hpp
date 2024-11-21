2f5a726ba35f2477e056786666e086aab54845c3
Eric Hernandez <herneri@protonmail.com>
2024-11-21 10:01:36 -0600

    feat: Add constants for general data

---
+++ constants.hpp
@@ -0,0 +1,9 @@
+#ifndef PIXINV_CONST_H
+#define PIXINV_CONST_H
+
+const char *PROGRAM_NAME = "pixinv";
+
+const int SCREEN_WIDTH = 800;
+const int SCREEN_HEIGHT = 800;
+
+#endif /* PIXINV_CONST_H */
